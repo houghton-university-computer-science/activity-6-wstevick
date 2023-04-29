8f42a995cd8afd9d690bd7897f37fc8aabacabf1
wstevick <12481632w@gmail.com>
2023-04-29 09:26:07 -0400

    add __str__ methods for models

--- db/models.py
+++ db/models.py
@@ -13,10 +13,16 @@ class Customer(models.Model):
     name = models.TextField(max_length=20)
     phone = models.TextField(max_length=14)
 
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     order_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.customer} order on {self.order_date}"
 
 
 class Product(models.Model):
@@ -25,8 +31,14 @@ class Product(models.Model):
     price = models.FloatField()
     product_id = models.CharField(max_length=5)
 
+    def __str__(self):
+        return self.name
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.order} - {self.product} - {self.quantity}"
